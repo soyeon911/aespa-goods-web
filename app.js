7de6d47638d393daf0559a65346741cbd111d65c
github-soyeon911 <githun-sypark911@yonsei.ac.kr>
2024-06-30 03:34:44 +0900

    f irst commit

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-'use strict';
+import React, { Component } from "react";
+import './main.css';	
 
-const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+class App extends Component {
+  render() {
+    return <div id="Soyeon Protfolio"></div>;
+  }
+  componentDidMount() { 
+    const FixRatio = () => {
+      const root = document.querySelector("#root");
+      const app = document.querySelector("#App");
+		
+      // 가로를 화면에 딱 맞게
+      let width = root.clientWidth;
+      let height = width * 0.5625;	// 1080 ÷ 1920 ≒ 0.5625
 
-    const className = document.body.className;
-    if(className== "light-theme"){
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
+      if (height > root.clientHeight) { // 설정된 세로 값이 화면보다 크다면
+        // 세로를 화면에 딱 맞게
+        height = root.clientHeight;
+        width = height * 1.7777;	// 1920 ÷ 1080 ≒ 1.7777
+      }
+	
+      // 설정한 값을 적용
+      app.style.width = `${width}px`;
+      app.style.height = `${height}px`;
+    };
 
-    console.log('current class name : ' + className);
-});
+    window.onresize = FixRatio; // 화면의 사이즈가 변할 때마다 호출
+    FixRatio(); // 맨 처음 실행 될 때도 호출
+  }
+}
+
+export default App;
